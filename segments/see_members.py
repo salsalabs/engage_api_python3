b5c33235f8112eabe7d034f0e7cc4f187b700d5d
Allen Leonard <aleonard@salsalabs.com>
2021-12-10 11:16:07 -0600

    Handles misising fields correctly.

--- segments/see_members.py
+++ segments/see_members.py
@@ -10,11 +10,25 @@ API.  The user provides the Engage token and a comma-separated list
 of segment IDs.  The output is a simple list of ID, segment and count.
 """
 
+def dumpJSON(x):
+    """
+    Generic function to return 'x' in formatted JSON.
+    
+    Parameters:
+    
+        x   any     thing to see
+    
+    Returns:
+
+        String containing 'x' as formatted JSON
+    """
+    return json.dumps(x, indent=4)
+
 def field(s, n, d):
     """
-    Engage does not equip JSON with empty fields. That means we need
-    to be careful when try to get fields from a dict parsed from JSON
-    provided by Engage.  
+    Engage does not put empty fields into the JSON from API calls.
+    That means we need to be careful when try to get fields from a
+    dict parsed from JSON provided by Engage.  
     
     This function checks to see if a field ('n') is in a dict ('s').
     If the answer is yes, then the value is returned.  If no, then
@@ -25,48 +39,30 @@ def field(s, n, d):
     else:
         return d
 
-def getSegment(args):
+def firstEmail(m, d):
     """
-    Retrieves the segment for 'segment-id' in args. 
-
+    Engage stores emails and phone numbers in the 'contacts' section
+    of a supporter record.  This function finds the first email.  If
+    there's not an email record, then we'll return a None.
+    
     Parameters:
 
-        args    dict    command-line arguments.
-
+        m   dict    supporter record
+        d   string  default value if an email is not equipped
+    
     Returns:
 
-        Returns a segment object if the segment-id is valid.
-        Returns None otherwise
-    
-    Errors:
-
-        Python errors are noisily fatal.
-        HTTP errors are noisily fatal.
+        string containing an email address, or
+        None if no email is configured
     """
-    endpoint = 'https://api.salsalabs.org/api/integration/ext/v1/segments/search'
-     # Specify the segment ID to read.
-    payload = {
-        "payload": {
-            "identifiers": [ args.segment_id],
-            "identifierType": "SEGMENT_ID"
-        }
-    }
-    response = postEngage(args, endpoint, payload)
-    print(json.dumps(response, indent=4))
 
-    # Extract the segments from the response payload.
-    rPayload = response["payload"]
-    segments =  rPayload["segments"]
+    if 'contacts' not in m:
+        return d
+    for c in m['contacts']:
+        if c['type'] == 'EMAIL':
+            return c['value']
+    return d
 
-    # Handle each segment. Okay, so there's just one, but we're
-    # iterating the list anyway...
-    for s in segments:
-        if "errors" in s:
-            showErrors(s)
-        elif "warnings" in s:
-            showWarnings(s)
-        else:
-            showContent(s)
 
 def postEngage(args, endpoint, payload):
     """
@@ -100,7 +96,7 @@ def postEngage(args, endpoint, payload):
     if (r.status_code != 200):
         logging.fatal(f"error: HTTP status code {r.status_code}")
         try:
-            s = json.dumps(json.loads(r.text), indent=4)
+            s = dumpJSON(json.loads(r.text))
         except:
             s = r.text
         logging.fatal(s)
@@ -120,30 +116,64 @@ def run(args):
     Errors:
         HTTP errors are also noisily fatal.
         Engage-specific errors are also noisily fatal.
-    """
+    """    
+    seeSegment(args)
 
     # offset = 0
     # count = args.count
     endpoint = 'https://api.salsalabs.org/api/integration/ext/v1/segments/members/search'
-    
-    segment = getSegment(args)
-    if segment is None:
-        return
-
-    # Specify the segment ID list and the type.
-    payload = {
+    requestPayload = {
         "payload": {
             "segmentId": args.segment_id,
             "offset": 0,
             "count": args.count
         }
     }
+    while requestPayload['payload']['count'] == args.count:
+        response = postEngage(args, endpoint, requestPayload)
 
-    # Process supporters from the payload
-    rPayload = response["payload"]
-    segments =  rPayload["segments"]
+        payload = response["payload"]
+        total = field(payload, 'total', '(None)')
+        offset = field(payload, 'offset', requestPayload['payload']['offset'])
+        count = field(payload, 'count', '(None)')
+        logging.info(f"{offset}/{total} {count} records")
 
-    # Handle each segment.
+        supporters = field(payload, 'supporters', None)
+        if supporters is None:
+            logging.warning("The segment does not have any members")
+            exit(1)
+        showMembers(supporters)
+        requestPayload['payload']['offset'] += count
+        requestPayload['payload']['count'] = count
+
+def seeSegment(args):
+    """
+    Display the segment for 'segment-id' in the command-line argments.
+
+    Parameters:
+
+        args    dict    command-line arguments
+    
+    Errors:
+
+        Python errors are noisily fatal.
+        HTTP errors are noisily fatal.
+    """
+    endpoint = 'https://api.salsalabs.org/api/integration/ext/v1/segments/search'
+    payload = {
+        "payload": {
+            "identifiers": [ args.segment_id],
+            "identifierType": "SEGMENT_ID"
+        }
+    }
+    response = postEngage(args, endpoint, payload)
+
+    # Extract the segments from the response payload.
+    payload = response["payload"]
+    segments = payload["segments"]
+
+    # Handle each segment. Okay, so there's just one, but we're
+    # iterating anyway...
     for s in segments:
         if "errors" in s:
             showErrors(s)
@@ -151,7 +181,6 @@ def run(args):
             showWarnings(s)
         else:
             showContent(s)
-            showMembers(s)
 
 def showErrors(s):
     """ Display segment-level errors."""
@@ -182,29 +211,24 @@ def showContent(s):
     totalMembers = field(s, 'totalMembers', '(None)')
     showCommon(s, f"{name:50} {totalMembers:>7}")
 
-def showMembers(s):
+def showMembers(members):
     """
     Show members as indented data.  Minimal info -- this is just a demo.
     
     Parameters:
 
-        s       dict      Segment object
+        members     list of dict    Supporter records for group members
     
     Errors:
 
         Python errors are noisy and fatal
 
     """
-    if "supporters" not in s:
-        showCommon(s, "Segment has no members")
-        return
-    a = s["supporters"]
-    logging.info(f"    Segment has {len(a)} supporters.")
-    for supporter in a:
-        supporterID = s['supporterID']
-        firstName = field(s, 'firstName', "")
-        lastName = field(s, 'lastName')
-        email = "email@example.org" #firstEmail(s)
+    for m in members:
+        supporterID = m['supporterId']
+        firstName = field(m, 'firstName', '')
+        lastName = field(m, 'lastName', '')
+        email = firstEmail(m, '(None)')
         logging.info(f"{supporterID} {firstName:<20} {lastName:<20} {email}")
 
 def main():
